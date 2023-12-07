fd333f2e2f5d662f58cacbac8c16e99707747aeb
Sebastián Flores <sfa120997@gmail.com>
2023-12-07 12:56:33 -0600

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   urlApiBovis: 'https://bovis-dev-api-up.azurewebsites.net/',
   redirectUri: 'https://witty-tree-0e061c510.4.azurestaticapps.net/',
   clientID: '66e9e892-18ae-49e4-aed0-0b4f7d997310',  
-  urlAuthority: 'https://login.microsoftonline.com/ea3fe555-2f74-402f-98aa-d517f141b8ff',
+  urlAuthority: 'https://login.microsoftonline.com/48986ced-d307-4c91-85be-933ccbcbaeb3',
   tokenUser: 'B0V1$-2023',
   tokenSecret: 'P4$w0RD_2023'
 };
