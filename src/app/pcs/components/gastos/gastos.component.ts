19abc133235515826d375830d7e84248b8ad69ee
Sebastián Flores <sfa120997@gmail.com>
2023-12-07 23:46:32 -0600

    New Button Removed

--- src/app/pcs/components/gastos/gastos.component.ts
+++ src/app/pcs/components/gastos/gastos.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { PcsService } from '../../services/pcs.service';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-gastos',
   templateUrl: './gastos.component.html',
-  styleUrls: ['./gastos.component.css']
+  styleUrls: ['./gastos.component.css'],
+  providers: [MessageService]
 })
 export class GastosComponent implements OnInit {
+
+  fb                = inject(FormBuilder)
+  messageService    = inject(MessageService)
+  pcsService        = inject(PcsService)
+  sharedService     = inject(SharedService)
 
   constructor() { }
 
   ngOnInit(): void {
+    
+    this.pcsService.cambiarEstadoBotonNuevo(false)
   }
 
 }
--- src/app/pcs/components/ppa-kpi/ppa-kpi.component.ts
+++ src/app/pcs/components/ppa-kpi/ppa-kpi.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { PcsService } from '../../services/pcs.service';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-ppa-kpi',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PpaKpiComponent implements OnInit {
 
+  fb                = inject(FormBuilder)
+  messageService    = inject(MessageService)
+  pcsService        = inject(PcsService)
+  sharedService     = inject(SharedService)
+
   constructor() { }
 
   ngOnInit(): void {
+    
+    this.pcsService.cambiarEstadoBotonNuevo(false)
   }
 
 }
