b2c78109e14196d2c60024773058895039b4e4c7
hunkabann <36930991+hunkabann@users.noreply.github.com>
2023-12-12 15:49:33 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
   //redirectUri: 'https://zealous-bush-0a2ce3c0f.4.azurestaticapps.net/',
-  redirectUri: 'https://gestion.bovis.mx/',
+  redirectUri: 'https://proud-sea-0d98bd40f.2.azurestaticapps.net/',
   clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11', 
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',
   tokenUser: 'B0V1$-2023',
