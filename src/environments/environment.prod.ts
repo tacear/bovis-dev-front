5e5c7c75f3f48a1f66e98cdde8093c423a8a1614
hunkabann <36930991+hunkabann@users.noreply.github.com>
2023-12-12 14:53:50 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   urlApiBovis: 'https://web-api-bovis.azurewebsites.net/',
   //redirectUri: 'https://zealous-bush-0a2ce3c0f.4.azurestaticapps.net/',
    redirectUri: 'https://gestion.bovis.mx/',
-  clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11',  
+ // clientID: '087f3ba6-2417-4f9d-a6ce-378095681e11',  
+   clientID: '49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',  
   urlAuthority: 'https://login.microsoftonline.com/49ef9ec4-2b0b-47a1-94f3-e82844c4f61f',
   tokenUser: 'B0V1$-2023',
   tokenSecret: 'P4$w0RD_2023'
