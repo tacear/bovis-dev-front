06126ab9fcb0b7831aee285d33449e38e6dfaf73
Sebastián Flores <sfa120997@gmail.com>
2024-01-31 23:06:09 -0600

    Fix

--- src/app/auditoria/auditoria.module.ts
+++ src/app/auditoria/auditoria.module.ts
@@ -21,6 +21,7 @@ import { TableModule } from 'primeng/table';
 import { SharedModule } from '../shared/shared.module';
 import { ProgresoComponent } from './components/progreso/progreso.component';
 import { ProgressBarModule } from 'primeng/progressbar';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { ProgressBarModule } from 'primeng/progressbar';
     FileUploadModule,
     TableModule,
     SharedModule,
-    ProgressBarModule
+    ProgressBarModule,
+    ToolbarModule
   ]
 })
 export class AuditoriaModule { }
