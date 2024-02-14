6c04d7ddb7efa09050be7510b13622d207670ae7
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-02-14 16:24:43 -0600

    Update auditoria.model.ts agrega fechaAuditoria a interface comentario
    se agrega fechaAuditoria a interface comentario

--- src/app/auditoria/models/auditoria.model.ts
+++ src/app/auditoria/models/auditoria.model.ts
@@ -112,6 +112,7 @@ export interface Comentario {
   fecha:            string;
   idTipoComentario: number;
   tipoComentario:   string;
+  fechaAuditoria:   string;
 }
 
 export interface TiposComentarioResponse {
