23d70533c31b50094fd70375267370c3a526d8ef
hunkabann <36930991+hunkabann@users.noreply.github.com>
2024-02-14 00:28:11 -0600

    Update comentarios-modal.component.ts agregar proximafecha y boton guardar
    se agrega proximafecha y boton guardar

--- src/app/auditoria/components/comentarios-modal/comentarios-modal.component.ts
+++ src/app/auditoria/components/comentarios-modal/comentarios-modal.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { MessageService } from 'primeng/api';
+import { MessageService,PrimeNGConfig } from 'primeng/api';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { SharedService } from 'src/app/shared/services/shared.service';
 import { Opcion } from 'src/models/general.model';
 import { AuditoriaService } from '../../services/auditoria.service';
 import { finalize, forkJoin } from 'rxjs';
-import { Comentario } from '../../models/auditoria.model';
+import { Comentario, FechaAuditoria } from '../../models/auditoria.model';
 import { FormBuilder, Validators } from '@angular/forms';
 import { TITLES, errorsArray } from 'src/utils/constants';
 
@@ -29,13 +29,23 @@ export class ComentariosModalComponent implements OnInit {
 
   tiposComentario: Opcion[] = []
   comentarios: { [key: string]: Comentario[] } = {};
+  FechaAuditoriaProx: { [key: string]: FechaAuditoria[] } = {};
+  
   form = this.fb.group({
     num_proyecto:       [null],
     comentario:         ['', Validators.required],
     id_tipo_comentario: ['', Validators.required]
   })
 
-  constructor() { }
+  formFecha = this.fb.group({
+    numProyecto:       [null],
+    fechaAuditoria:         ['', Validators.required],
+    
+  })
+
+  constructor(
+    private configCalendar: PrimeNGConfig
+  ) { }
 
   ngOnInit(): void {
     if(this.config.data) {
@@ -52,6 +62,8 @@ export class ComentariosModalComponent implements OnInit {
     this.sharedService.cambiarEstado(true)
 
     this.form.patchValue({ num_proyecto: this.numProyecto })
+    this.formFecha.patchValue({ numProyecto: this.numProyecto })
+
 
     forkJoin([
       this.auditoriaService.getTiposComentario(),
@@ -112,6 +124,31 @@ export class ComentariosModalComponent implements OnInit {
 
   }
 
+  guardarFechaAuditoria() {
+    
+    if(!this.formFecha.valid) {
+      this.formFecha.markAllAsTouched()
+      return
+    }
+
+    this.sharedService.cambiarEstado(true)
+
+    this.auditoriaService.agregarFechaProxAuditoria(this.formFecha.value)
+      .pipe(finalize(() => this.sharedService.cambiarEstado(false)))
+      .subscribe({
+        next: ({data}) => {
+          this.messageService.add({severity: 'success', summary: TITLES.success, detail: 'La Proxima Fecha de Auditoria ha sido agregada.'})
+          this.FechaAuditoriaProx[this.formFecha.value.fechaAuditoria].push({
+            numProyecto:      this.formFecha.value.numProyecto,
+            fechaAuditoria:            this.formFecha.value.fechaAuditoria
+          })
+          this.formFecha.reset()
+        },
+        error: (err) => this.messageService.add({severity: 'error', summary: TITLES.error, detail: err.error})
+      })
+
+  }
+
   esInvalido(campo: string): boolean {
     return this.form.get(campo).invalid && 
             (this.form.get(campo).dirty || this.form.get(campo).touched)
@@ -122,6 +159,69 @@ export class ComentariosModalComponent implements OnInit {
 
     errorsArray.forEach((error) => {
       if(this.form.get(campo).hasError(error.tipo))
+        mensaje = error.mensaje.toString()
+    })
+
+    return mensaje
+  }
+
+  getConfigCalendar() {
+    this.configCalendar.setTranslation({
+      firstDayOfWeek: 1,
+      dayNames: [
+        'domingo',
+        'lunes',
+        'martes',
+        'miércoles',
+        'jueves',
+        'viernes',
+        'sábado',
+      ],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: [
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre',
+      ],
+      monthNamesShort: [
+        'ene',
+        'feb',
+        'mar',
+        'abr',
+        'may',
+        'jun',
+        'jul',
+        'ago',
+        'sep',
+        'oct',
+        'nov',
+        'dic',
+      ],
+      today: 'Hoy',
+      clear: 'Limpiar',
+    });
+  }
+
+  esInvalidoFecha(campo: string): boolean {
+    return this.formFecha.get(campo).invalid && 
+            (this.formFecha.get(campo).dirty || this.formFecha.get(campo).touched)
+  }
+
+  obtenerMensajeErrorFecha(campo: string): string {
+    let mensaje = ''
+
+    errorsArray.forEach((error) => {
+      if(this.formFecha.get(campo).hasError(error.tipo))
         mensaje = error.mensaje.toString()
     })
 
