1179a17c05f1ceb26bd50ea1b5b03e933d716a42
elbaltezar <norberto.baltezar@gmail.com>
2024-02-28 23:02:23 -0600

    Update formating

--- src/app/pcs/components/crear-etapa/crear-etapa.component.ts
+++ src/app/pcs/components/crear-etapa/crear-etapa.component.ts
@@ -16,39 +16,39 @@ import { finalize } from 'rxjs';
 })
 export class CrearEtapaComponent implements OnInit {
 
-  ref               = inject(DynamicDialogRef)
-  config            = inject(DynamicDialogConfig)
-  primeConfig       = inject(PrimeNGConfig)
-  messageService    = inject(MessageService)
-  sharedService     = inject(SharedService)
-  pcsService        = inject(PcsService)
-  fb                = inject(FormBuilder)
+  ref = inject(DynamicDialogRef)
+  config = inject(DynamicDialogConfig)
+  primeConfig = inject(PrimeNGConfig)
+  messageService = inject(MessageService)
+  sharedService = inject(SharedService)
+  pcsService = inject(PcsService)
+  fb = inject(FormBuilder)
 
   fechaMinima: Date
   fechaMaxima: Date
 
   form = this.fb.group({
     num_proyecto: [null],
-    orden:        [1],
-    nombre_fase:  ['', Validators.required],
+    orden: [1],
+    nombre_fase: ['', Validators.required],
     fecha_inicio: ['', Validators.required],
-    fecha_fin:    ['', Validators.required]
+    fecha_fin: ['', Validators.required]
   })
 
   constructor() { }
 
   ngOnInit(): void {
-    
+
     this.getConfigCalendar();
 
     const data = this.config.data
-    if(data) {
-      if(data.numProyecto) {
+    if (data) {
+      if (data.numProyecto) {
         this.form.patchValue({
           num_proyecto: data.numProyecto
         })
       }
-      if(data.fechaInicio && data.fechaFin) {
+      if (data.fechaInicio && data.fechaFin) {
         this.fechaMinima = startOfMonth(data.fechaInicio)
         this.fechaMaxima = startOfMonth(data.fechaFin)
       }
@@ -56,7 +56,7 @@ export class CrearEtapaComponent implements OnInit {
   }
 
   guardar() {
-    if(!this.form.valid) {
+    if (!this.form.valid) {
       this.form.markAllAsTouched()
       return
     }
@@ -66,20 +66,20 @@ export class CrearEtapaComponent implements OnInit {
     const body = {
       ...this.form.value,
       fecha_inicio: this.form.value.fecha_inicio ? format(new Date(this.form.value.fecha_inicio), 'Y-MM-dd') : null,
-      fecha_fin:    this.form.value.fecha_fin ? format(new Date(this.form.value.fecha_fin), 'Y-MM-dd') : null
+      fecha_fin: this.form.value.fecha_fin ? format(new Date(this.form.value.fecha_fin), 'Y-MM-dd') : null
     }
 
     this.pcsService.agregarEtapa(body)
       .pipe(finalize(() => this.sharedService.cambiarEstado(false)))
       .subscribe({
-        next: ({data}) => {
-          this.messageService.add({severity: 'success', summary: TITLES.success, detail: 'La etapa ha sido agregada.'})
-          this.ref.close({etapa: data})
+        next: ({ data }) => {
+          this.messageService.add({ severity: 'success', summary: TITLES.success, detail: 'La etapa ha sido agregada.' })
+          this.ref.close({ etapa: data })
         },
-        error: (err) => this.messageService.add({severity: 'error', summary: TITLES.error, detail: err.error})
+        error: (err) => this.messageService.add({ severity: 'error', summary: TITLES.error, detail: err.error })
       })
   }
-  
+
   getConfigCalendar() {
     this.primeConfig.setTranslation({
       firstDayOfWeek: 1,
@@ -94,15 +94,15 @@ export class CrearEtapaComponent implements OnInit {
   }
 
   esInvalido(campo: string): boolean {
-    return this.form.get(campo).invalid && 
-            (this.form.get(campo).dirty || this.form.get(campo).touched)
+    return this.form.get(campo).invalid &&
+      (this.form.get(campo).dirty || this.form.get(campo).touched)
   }
 
   obtenerMensajeError(campo: string): string {
     let mensaje = ''
 
     errorsArray.forEach((error) => {
-      if(this.form.get(campo).hasError(error.tipo))
+      if (this.form.get(campo).hasError(error.tipo))
         mensaje = error.mensaje.toString()
     })
 
